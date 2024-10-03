bb9e156f3cda73b3a8067a1766d1f2700c7ee217
arseneruben <fopoar@gmail.com>
2024-10-03 10:42:26 -0400

    Exo6 avec les listes

--- atelier1/exercice6_with_list.py
+++ atelier1/exercice6_with_list.py
@@ -1,25 +1,14 @@
 mois = input("Donner un mois de votre choix: ")
-# listeMois = ["Janvier","Fevrier" , "Mars","Mai","Juin","Juillet","Aout" ,"Septembre",
+listeMois = ["Janvier","Fevrier" , "Mars","Mai","Juin","Juillet","Aout" ,"Septembre",
   "Octobre", "Novembre"   ,"Decembre"]
-# while (mois not in listeMois): on peut aussi proceder a l'aide d'une liste
-while(not(mois=="Janvier" or
-   mois=="Fevrier" or
-   mois=="Mars" or
-   mois=="Avril" or
-   mois=="Mai" or
-   mois=="Juin" or
-   mois=="Juillet" or
-   mois=="Aout" or
-   mois=="Septembre" or
-   mois=="Octobre" or
-   mois=="Novembre" or
-   mois=="Decembre")):
+listeMois31Jours = ["Janvier", "Mars","Mai","Juillet","Aout","Octobre","Decembre"]
+
+while(mois not in listeMois):
     print("Warning: Entrer un mois de l'annee!!!")
     mois = input("Donner un mois de votre choix: ")
 
     
-if(mois=="Janvier" or mois=="Mars"or mois=="Mai"or mois=="Juillet"or mois=="Aout"
-   or mois=="Octobre" or mois=="Decembre"):
+if(mois in listeMois31Jours):
     jours = 31
 elif(mois=="Fevrier"):
     while True:
