74fc45a531e0c3c8ca2236adc10f27fa65bb00cd
arseneruben <fopoar@gmail.com>
2024-10-03 09:21:52 -0400

    Exo1 termine

---
+++ atelier1/exercice1.py
@@ -0,0 +1,19 @@
+A= input('Veuillez saisir la valeur de A : ')
+B= input('Veuillez saisir la valeur de B : ')
+C= input('Veuillez saisir la valeur de C : ')
+print ('A=', A)
+print ('B=', B)
+print ('C=', C)
+temps=A
+A=C
+C=B
+B=temps
+print ('voici les nouveles valeures de A,B,C')
+print ('A=', A)
+print ('B=', B)
+print ('C=', C)
+
+
+
+
+
