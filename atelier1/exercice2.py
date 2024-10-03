b7d40ca576d59b018942b5788c4c3e78ab201146
arseneruben <fopoar@gmail.com>
2024-10-03 09:38:11 -0400

    Exo2 termine

---
+++ atelier1/exercice2.py
@@ -0,0 +1,17 @@
+import math
+
+while True:
+    try:
+        nb = float(input("Donner une valeur numerique: "))
+        print("Le cube de {} est {} ".format( nb, nb*nb*nb))
+        break
+    except ValueError:
+             print("Donner une valeur numerique!")
+
+            
+
+
+
+
+
+
